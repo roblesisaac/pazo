3ac791f43fb79c8193eed477135b70546632a674
roblesisaac <irobles1030@gmail.com>
2018-03-06 17:42:35 -0800

    Update nestedWhile.js

--- nestedWhile.js
+++ nestedWhile.js
@@ -9,9 +9,19 @@ public class MyClass {
       
       String playgame = "y";
       while(playgame.equals("y")) {
+        System.out.println("Guess a number between 1 and 10.");
         int count = 1;
-        count = 2;
-        playgame = "n";
+        int num = rnd.nextInt(10) + 1;
+        int guess = scan.nextInt();
+        while(guess != num) {
+          System.out.println("Guess again.");
+          guess = scan.nextInt();
+          count++;
+        }
+        System.out.println("took " + count + " times.");
+        System.out.println("Play again? (y/n)");
+        scan.nextLine();
+        playgame = scan.nextLine();
       } 
       
       System.out.println("Thanks for playing!");
