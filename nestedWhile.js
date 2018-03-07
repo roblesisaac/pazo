316658f3e56583776aff72a5596027bedfabfe33
roblesisaac <irobles1030@gmail.com>
2018-03-06 18:06:44 -0800

    Update nestedWhile.js

--- nestedWhile.js
+++ nestedWhile.js
@@ -13,14 +13,8 @@ public class MyClass {
         int count = 1;
         int num = rnd.nextInt(10) + 1;
         int guess = scan.nextInt();
-        while(guess != num) {
-          System.out.println("Guess again.");
-          guess = scan.nextInt();
-          count++;
-        }
         System.out.println("took " + count + " times.");
         System.out.println("Play again? (y/n)");
-        scan.nextLine();
         playgame = scan.nextLine();
       } 
       
