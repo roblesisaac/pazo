75590e1d38c1703a8a4e1afd757217ea27177670
roblesisaac <irobles1030@gmail.com>
2018-03-10 15:31:48 -0800

    Update whileDifferenceOfLastTwoNumber.js

--- week2/whileDifferenceOfLastTwoNumber.js
+++ week2/whileDifferenceOfLastTwoNumber.js
@@ -1,17 +1,19 @@
-  import java.util.*;
+import java.util.*;
 import java.lang.*;
 import java.io.*;
  
 public class MyClass {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
-      
-        int previous = 1;
-        int current = 2;
+
+        System.out.println("Pick a number.");
+        int previous = scan.nextInt();
+        System.out.println("Pick a number.");
+        int current = scan.nextInt();
         while(Math.abs(current-previous) <= 10) {
             System.out.println("Pick a number.");
-            previous = current; // storing/saving last current number in the previous variable
-            current = scan.nextInt(); // resets current to whatever value user inputs
+            previous = current;
+            current = scan.nextInt();
         }
         System.out.println("The difference between the last two numbers enter is greater than 10.");
       
