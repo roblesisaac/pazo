1826135347bae5d7b8abee6dddbf488a7e9289ea
roblesisaac <irobles1030@gmail.com>
2018-03-10 14:26:53 -0800

    Update nestedForAlphabet.js

--- week2/nestedForAlphabet.js
+++ week2/nestedForAlphabet.js
@@ -1,1 +1,11 @@
-sfd
+
+public class MyClass {
+    public static void main(String args[]) {
+        char[] alpha = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        String message = "";
+        for(int count=0; count<7; count++) {
+          message += alpha[count];
+          System.out.println(message);
+        }
+    }
+}
