dfb88caf45fb98a8e9681a736701ba1785b12def
rmitsantos <37252232+rmitsantos@users.noreply.github.com>
2018-03-10 15:04:43 -0800

    Update nestedForAlphabet.js

--- week2/nestedForAlphabet.js
+++ week2/nestedForAlphabet.js
@@ -6,7 +6,7 @@ public class MyClass {
     public static void main(String args[]) {
         
         String message = "";
-        for(int count=65; count<91; count++) {
+        for(int count=65; count<91; count++) { // 91 is equal to 26 times in loop
           message += (char)(count); // same as message = message +(char)(count)
           System.out.println(message);
         }
