47828c300ba75cbad57aa56ac24714fd5da371a9
rmitsantos <37252232+rmitsantos@users.noreply.github.com>
2018-03-11 15:12:46 -0700

    Update Temporary.js

--- week2/Temporary.js
+++ week2/Temporary.js
@@ -1,40 +1,57 @@
-package dowhilepracticeexam2;
-import java.util.Scanner;
-import java.util.Random;
-
-/**
- *
- * @author Ryan
- */
-public class DoWhilePracticeExam2 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Random rnd = new Random();
+        Scanner in = new Scanner(System.in); // creates a scanner isntacne
+
+        Random rnd = new Random(); // creates a random instance
+
         
+
         int min = 65;
+
         int max = 90;
+
         
+
         int rNumber = rnd.nextInt((max-min)+1) + min;
+
         char rChar = (char)(rNumber); 
+
         
-        System.out.println("Secret letter is " + rChar);
+
         
+
+        
+
         System.out.println("Guess the secret uppercase letter");
+
         String guess = in.nextLine();
+        guess = guess.toUpperCase();
+        
+        System.out.println("You guessed " + guess); 
+
         do {
+             
              System.out.println("Sorry you guessed the wrong letter, the secret letter preceeds the letter you guessed. Guess again.");
+             
+             
              guess = in.nextLine();
+             guess = guess.toUpperCase();
+
         } while(!guess.equals(rChar));
+
         
+
         
+
       
+
       
+
                 
+
                
+
     }
+
     
+
 }
