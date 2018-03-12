9ebb6fdadac86179399aa8ad3fed5c297fa0869d
rmitsantos <37252232+rmitsantos@users.noreply.github.com>
2018-03-11 21:32:39 -0700

    Update examQuestion2.js

--- week2/examQuestion2.js
+++ week2/examQuestion2.js
@@ -4,24 +4,28 @@ Using a while loop generate the sum and compute the average of random numbers 
 generated random number matches the number input by the user. Do not include this number as part of your sum. 
 Make sure you prime your loop properly.
 
- Scanner in = new Scanner (System.in);
-        Random rnd = new Random();
+  Scanner in = new Scanner (System.in);
+  Random rnd = new Random();
         
         System.out.println("Pick a number between 1 and 1000");
-       
         int answer = in.nextInt();
+
         int i = 0;
         int sum = 0;
-        
-        while(i < answer){
+       
+       while(i < answer){
+
             int num = rnd.nextInt(999) + 1;
+
             sum += num; 
-           
+            i++;
+
         }
+
            int x = sum / i;
+
           System.out.println("The average is " + x);
-        
-        
+
     }
-    
+
 }
