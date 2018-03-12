2a5e6d37b7249b223f8b553f58f048fdf9b10f30
roblesisaac <irobles1030@gmail.com>
2018-03-11 19:38:09 -0700

    Update Temporary.js

--- week2/Temporary.js
+++ week2/Temporary.js
@@ -1,36 +1,36 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
- 
+
 public class MyClass {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in); // creates a scanner isntacne
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in); // creates a scanner isntacne
 
-        Random rnd = new Random(); // creates a random instance
-        int min = 65; // is A in ascii
-        int max = 90; // is Z in ascii
-        int rNumber = rnd.nextInt((max-min)+1) + min;
-        char rChar = (char)(rNumber);
-        char guess = (char)(60); 
-        int x = guess;
-     
+    Random rnd = new Random(); // creates a random instance
+    int min = 65; // is A in ascii
+    int max = 90; // is Z in ascii
+    int rNumber = rnd.nextInt((max - min) + 1) + min;
+    char rChar = (char)(rNumber);
+    char guess = (char)(60);
+    int x = guess;
 
-        do { 
-            if(guess != (rChar)) {
-                System.out.println("Guess the secret uppercase letter (it's " + rChar + ")"); 
-            } else {
-                System.out.println("Sorry you guessed the wrong letter, the secret letter preceeds the letter you guessed. Guess again.");   
-            }
-            guess = in.next().charAt(0); // how to get char from next line (input)
-           x = guess;
-         if(x > rNumber ){
-          System.out.println("Your guess came after the secret letter, try guessing a letter that comes before.");
-         } else if(x != rNumber){
-          System.out.println("Your guess comes before the secret letter, try guessing a latter letter.");
-                              }
 
-        } while(guess != rChar);        
-        System.out.println("You guessed correctly!");
-    }
+    do {
+      if (guess != (rChar)) {
+        System.out.println("Guess the secret uppercase letter (it's " + rChar + ")");
+      } else {
+        System.out.println("Sorry you guessed the wrong letter, the secret letter preceeds the letter you guessed. Guess again.");
+      }
+      guess = in .next().charAt(0); // how to get char from next line (input)
+      x = guess;
+      if (x > rNumber) {
+        System.out.println("Your guess came after the secret letter, try guessing a letter that comes before.");
+      } else if (x != rNumber) {
+        System.out.println("Your guess comes before the secret letter, try guessing a latter letter.");
+      }
+
+    } while (guess != rChar);
+    System.out.println("You guessed correctly!");
+  }
 
 }
