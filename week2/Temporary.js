e553e0ab26c718ff43920927db618e96475d3e56
roblesisaac <irobles1030@gmail.com>
2018-03-11 17:48:51 -0700

    Update Temporary.js

--- week2/Temporary.js
+++ week2/Temporary.js
@@ -11,27 +11,17 @@ public class MyClass {
         
 
         int min = 65;
-
         int max = 90;
-
-        
-
         int rNumber = rnd.nextInt((max-min)+1) + min;
-
         char rChar = (char)(rNumber); 
 
         System.out.println("Guess the secret uppercase letter (it's " + rChar + ")");
 
         String guess = in.nextLine();
-        guess = guess.toUpperCase();
-        
-        System.out.println("You guessed " + guess); 
 
         do {
              System.out.println(guess.equals(rChar)); 
              // System.out.println("Sorry you guessed the wrong letter, the secret letter preceeds the letter you guessed. Guess again.");
-             
-             
              guess = in.nextLine();
 
         } while(!guess.equals(rChar));
