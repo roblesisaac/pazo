f919a18138bd545815fe1e1937c9003d2efeb349
roblesisaac <irobles1030@gmail.com>
2018-03-11 21:07:55 -0700

    Update examQuestion2.js

--- week2/examQuestion2.js
+++ week2/examQuestion2.js
@@ -1,16 +1,17 @@
-Write a program that will ask the user for a number between 1 and 1000. Using a while loop generate the sum and compute the average of random numbers in the range of 1 to 1000 until a generated random number matches the number input by the user. Do not include this number as part of your sum. 
+Write a program that will ask the user for a number between 1 and 1000. 
+Using a while loop generate the sum and compute the average of random numbers in the range of 1 to 1000 until a 
+generated random number matches the number input by the user. Do not include this number as part of your sum. 
 Make sure you prime your loop properly.
 
   Scanner in = new Scanner (System.in);
         Random rnd = new Random();
         
-        
-        
         System.out.println("Pick a number between 1 and 1000");
        
         int answer = in.nextInt();
+        int i = 0;
         
-        while(answer ){
+        while(i < answer){
             
         }
         
