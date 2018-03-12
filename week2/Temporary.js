5f9143fb5750e9a72b1d897de1869fc5a7774c45
rmitsantos <37252232+rmitsantos@users.noreply.github.com>
2018-03-11 18:44:31 -0700

    Update Temporary.js

--- week2/Temporary.js
+++ week2/Temporary.js
@@ -1,4 +1,4 @@
-!import java.util.*;
+ !import java.util.*;
 import java.lang.*;
 import java.io.*;
  
@@ -7,8 +7,8 @@ public class MyClass {
         Scanner in = new Scanner(System.in); // creates a scanner isntacne
 
         Random rnd = new Random(); // creates a random instance
-        int min = 65;
-        int max = 90;
+        int min = 65; // is A in ascii
+        int max = 90; // is Z in ascii
         int rNumber = rnd.nextInt((max-min)+1) + min;
         char rChar = (char)(rNumber);
         char guess = (char)(60);
@@ -18,8 +18,9 @@ public class MyClass {
                 System.out.println("Guess the secret uppercase letter (it's " + rChar + ")"); 
             } else {
                 System.out.println("Sorry you guessed the wrong letter, the secret letter preceeds the letter you guessed. Guess again.");   
-            };
-            guess = in.next().charAt(0);
+            }
+            guess = in.next().charAt(0); // how to get char from next line (input)
+            int = guess;
         } while(guess != rChar);        
         System.out.println("You guessed correctly!");
     }
