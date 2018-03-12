d31fca03add275624ebdd7643b138b6dde749449
rmitsantos <37252232+rmitsantos@users.noreply.github.com>
2018-03-11 20:39:49 -0700

    Update exam.js

--- week2/exam.js
+++ week2/exam.js
@@ -1,3 +1,5 @@
+*
+
 Scanner in = new Scanner(System.in);
         
         System.out.println("Enter some text.");
@@ -8,3 +10,19 @@ Scanner in = new Scanner(System.in);
        for( int i =0; i < text.length(); i++ );{
        char c = text.charAt(i);
     }
+
+/*Using a for loop count the number of spaces that appear in a string
+Example:
+Enter some text
+this is a test
+Your text has 3 spaces in it
+tset a si siht
+Please not that you can find the length of a string by using the length function
+string str = "hello";
+cout << str.length();   // prints 5
+Also you can get a character in a string by providing an index to the at functio
+string str = "hello";
+cout << str.at(2) << endl;  // outputs the character l
+finally a space character is nothing more than empty quotes with a space in between:
+char ch = ' ';
+ 
