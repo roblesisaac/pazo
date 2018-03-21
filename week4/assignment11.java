8bdcf54112b16ed185cd7ce399cf49f162ac560a
roblesisaac <irobles1030@gmail.com>
2018-03-21 16:35:53 -0700

    Update assignment11.java

--- week4/assignment11.java
+++ week4/assignment11.java
@@ -11,29 +11,29 @@ import java.util.Scanner;
 
 public class Lab11 {
 
- Scanner in = new Scanner(System.in);
- 
- public sumConsonants() {
-     
-     System.out.println("Enter two uppercase leters.");
-     
-     String s = in.nextLine();
-   // int x = Integer.parseInt(s); // converts string into int
-    letter1 = in.nextLine();
-    letter2 = in.nextLine();
-    
-    for( ; ;)
-  
-     
-     return (char)(letter1 + letter2); // sum of ascii letters
-     
- }
-    public static void main(String[] args) {
-        
-        Lab11 console = new Lab11();
-        int x = console.sumConsonants();
-        System.out.prtinln("The sum of the consonants is " + x);
-        
-    }
-    
+  Scanner in = new Scanner(System.in);
+
+  public sumConsonants() {
+
+    System.out.println("Enter two uppercase leters.");
+
+    String s = in .nextLine();
+    // int x = Integer.parseInt(s); // converts string into int
+    letter1 = in .nextLine();
+    letter2 = in .nextLine();
+
+    for (;;)
+
+
+      return (char)(letter1 + letter2); // sum of ascii letters
+
+  }
+  public static void main(String[] args) {
+
+    Lab11 console = new Lab11();
+    int x = console.sumConsonants();
+    System.out.prtinln("The sum of the consonants is " + x);
+
+  }
+
 }
