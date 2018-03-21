b96b1528dd8f48aa9f854411b07419ec5233af37
roblesisaac <irobles1030@gmail.com>
2018-03-21 16:54:22 -0700

    Update assignment11.java

--- week4/assignment11.java
+++ week4/assignment11.java
@@ -18,17 +18,17 @@ public class Lab11 {
     System.out.println("Enter two uppercase leters.");
 
     String s = in.nextLine();
-    // int x = Integer.parseInt(s); // converts string into int
-    letter1 = in.nextLine(); // for the letters you want to use s.charAt()
+    char letter1 = s.charAt(0); // replace 0 with appropriate index for letter2
    // i found this on lines 27-28 on instructions
     letter2 = in.nextLine();
+   
    // to convert letter1 try int l1 = (int) letter1
    // i found this on lines 17-20 on instructions
 
     for (;;) // get rid of this for now so function will execute
 
     return (char)(letter1 + letter2); // sum of ascii letters
-   // instead of returning this try returning the integers from the converted letters (line 25) to see if your on the right track
+   // instead of returning this try returning the integers from the converted letters we got on line 25 to see if your on the right track
 
   }
   public static void main(String[] args) {
