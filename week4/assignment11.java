cfeb0ae3ae4c19885179c0a953734f4b6bef5ac1
rmitsantos <37252232+rmitsantos@users.noreply.github.com>
2018-03-21 18:25:50 -0700

    Update assignment11.java

--- week4/assignment11.java
+++ week4/assignment11.java
@@ -9,33 +9,40 @@ package lab11;
 import java.util.Scanner;
 
 
-public class Lab11 { //had to define the method with int
+public class Lab11 {
 
-  Scanner in = new Scanner(System.in);
-
-  public int sumConsonants() { // i think definition goes here - not line 12
-    System.out.println("Enter two uppercase leters.");
-    String s = in.nextLine();
-    char letter1 = s.charAt(0);
-    char letter2 = s.charAt(1);
+ Scanner in = new Scanner(System.in);
+ 
+ public int sumConsonants() {
+     
+     System.out.println("Enter two uppercase leters.");
+     
+     String s = in.nextLine();
+     char letter1 = s.charAt(0);
+     char letter2 = s.charAt(1);
+   // int x = Integer.parseInt(s); // converts string into int
     int l1 = (int)letter1;
     int l2 = (int)letter2;
-
-   // ** now you need to set up your for loop **
-   // so you need to find the amount of times to loop which will be the difference between l1 and l2.
-   // the result must be positive - i think you could use Math.abs ??
-   // I think thats the point of his instructions here: Your method must allow for either argument to be larger. For instance, passing A, C should yield the same as passing C, A.
-   // ** then **
-   //You must also insure that the arguments to this method are upper case characters. If either argument is not you should return a -1 from the method;
-   // if(Character.isUpperCase(ch)) then you would return System.out.println("Upper Case");
-   
+    
+    if(Character.isUpperCase(letter1)) {
+        System.out.println("You must enter an uppercase letter.");
+        
+        if(!Character.isUpperCase(letter2)) {
+            System.out.println("You must enter an uppercase letter.");
+        }
+    }
+    else {
+        return sumConsonants();
+    }
     return (l1 + l2); // sum of ascii letters
-  }
-  
-  public static void main(String[] args) {
-    Lab11 console = new Lab11();
-    int x = console.sumConsonants();
-    System.out.println("The sum of the consonants is " + x);
-  }
-
+     
+ }
+    public static void main(String[] args) {
+        
+        Lab11 console = new Lab11();
+        int x = console.sumConsonants();
+        System.out.println("The sum of the consonants is " + x);
+        
+    }
+    
 }
