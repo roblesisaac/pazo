7e7b98e2ee64378a7a86d0c325a5723deebab75d
rmitsantos <37252232+rmitsantos@users.noreply.github.com>
2018-03-21 17:36:12 -0700

    Update assignment11.java

--- week4/assignment11.java
+++ week4/assignment11.java
@@ -9,7 +9,7 @@ package lab11;
 import java.util.Scanner;
 
 
-public class Lab11 {
+public int class Lab11 { //had to define the method with int
 
   Scanner in = new Scanner(System.in);
 
@@ -18,16 +18,20 @@ public class Lab11 {
     System.out.println("Enter two uppercase leters.");
 
     String s = in.nextLine();
-    char letter1 = s.charAt(0); // replace 0 with appropriate index for letter2
+    char letter1 = s.charAt(0); //updated
+    char letter2 = s.charAt(1); //updated
+   // replace 0 with appropriate index for letter2
    // i found this on lines 27-28 on instructions
-    letter2 = in.nextLine();
+    int l1 = (int)letter1; //updated
+    int l2 = (int)letter2; //updated
    
    // to convert letter1 try int l1 = (int) letter1
    // i found this on lines 17-20 on instructions
 
-    for (;;) // get rid of this for now so function will execute
+   // for (;;) // get rid of this for now so function will execute
 
-    return (char)(letter1 + letter2); // sum of ascii letters
+    return (l1 + l2); //updated 
+   // sum of ascii letters
    // instead of returning this try returning the integers from the converted letters we got on line 25 to see if your on the right track
 
   }
