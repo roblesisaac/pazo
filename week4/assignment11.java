4be6ca29bb60bbf90db116e863e3bb8a3cc4ccab
roblesisaac <irobles1030@gmail.com>
2018-03-21 17:56:22 -0700

    Update assignment11.java

--- week4/assignment11.java
+++ week4/assignment11.java
@@ -9,38 +9,24 @@ package lab11;
 import java.util.Scanner;
 
 
-public int class Lab11 { //had to define the method with int
+public class Lab11 { //had to define the method with int
 
   Scanner in = new Scanner(System.in);
 
-  public sumConsonants() {
-
+  public int sumConsonants() { // i think definition goes here - not line 12
     System.out.println("Enter two uppercase leters.");
-
     String s = in.nextLine();
-    char letter1 = s.charAt(0); //updated
-    char letter2 = s.charAt(1); //updated
-   // replace 0 with appropriate index for letter2
-   // i found this on lines 27-28 on instructions
-    int l1 = (int)letter1; //updated
-    int l2 = (int)letter2; //updated
-   
-   // to convert letter1 try int l1 = (int) letter1
-   // i found this on lines 17-20 on instructions
-
-   // for (;;) // get rid of this for now so function will execute
-
-    return (l1 + l2); //updated 
-   // sum of ascii letters
-   // instead of returning this try returning the integers from the converted letters we got on line 25 to see if your on the right track
-
+    char letter1 = s.charAt(0);
+    char letter2 = s.charAt(1);
+    int l1 = (int)letter1;
+    int l2 = (int)letter2;
+    return (l1 + l2); // sum of ascii letters
   }
+  
   public static void main(String[] args) {
-
     Lab11 console = new Lab11();
     int x = console.sumConsonants();
-    System.out.prtinln("The sum of the consonants is " + x);
-
+    System.out.println("The sum of the consonants is " + x);
   }
 
 }
