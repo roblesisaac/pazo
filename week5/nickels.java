287e1bc5721f916a54ae69cc69141a04b6082665
rmitsantos <37252232+rmitsantos@users.noreply.github.com>
2019-01-14 20:48:46 -0800

    Create nickels.java

---
+++ week5/nickels.java
@@ -0,0 +1,30 @@
+/*
+ * Name : Ryan Santos
+ * ID : 872
+ * Date :  1/14  /2019
+ * Description :
+ */
+package journal1c;
+
+import java.util.Scanner;
+public class Journal1c 
+{
+
+    
+    public static void main(String[] args) 
+    {
+       Scanner in = new Scanner(System.in);
+       int nickels;
+       
+       System.out.println("Enter your Nickels");
+       nickels = in.nextInt();
+        
+       int dollars = nickels / 20;
+       int change = nickels % 20;
+       int change1 = change / 2;
+               
+       System.out.println("You have $" + dollars + "." + change1);
+        
+    }
+    
+}
