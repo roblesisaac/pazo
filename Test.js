3857828307a767a55aa78a69f1ea51d164783b1d
roblesisaac <irobles1030@gmail.com>
2020-10-31 15:05:03 -0700

    Update Test.js

--- Test.js
+++ Test.js
@@ -246,18 +246,6 @@ function addMethodToArray(name, fn) {
 
     }
 
-    function proper(str) {
-
-        str = str.toLowerCase();
-
-        let spl = str.split("");
-
-        spl[0] = spl[0].toUpperCase();
-
-        return spl.join("");
-
-    }
-
     function renameProp(obj, oldProp, newProp) {
 
         var newObj = {};
@@ -414,9 +402,21 @@ function addMethodToArray(name, fn) {
 
         strDelimiter = strDelimiter || ",";
 
-        var objPattern = new RegExp(( // Delimiters.  "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +  // Quoted fields.  "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +  // Standard fields.  "([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");  var arrData = [[]];  var arrMatches = null;  while (arrMatches = objPattern.exec(stringData)) {    var strMatchedDelimiter = arrMatches[1];    if (strMatchedDelimiter.length && (strMatchedDelimiter != strDelimiter)) {      arrData.push([]);    }    if (arrMatches[2]) {      var strMatchedValue = arrMatches[2].replace(      new RegExp("\"\"", "g"), "\"");    } else {      var strMatchedValue = arrMatches[3];    }    arrData[arrData.length - 1].push(strMatchedValue);  }  return (arrData);}function convertCsvToJson(csv, delimeter) {  var array = CSVToArrays(csv, delimeter);  var objArray = [];  for (var i = 1; i < array.length; i++) {    objArray[i - 1] = {};    for (var k = 0; k < array[0].length && k < array[i].length; k++) {      var key = array[0][k];      objArray[i - 1][key] = array[i][k]    }  }  return objArray;}if (!String.prototype.includes) {  String.prototype.includes = function() {    "use strict";    return String.prototype.indexOf.apply(this, arguments) !== -1;  };}if (!String.prototype.excludes) {  String.prototype.excludes = function() {    "use strict";    return String.prototype.indexOf.apply(this, arguments) === -1;  };}if (typeof String.prototype.parseFunction != "function") {  String.prototype.parseFunction = function () {    var funcReg = /function *\(([^()]*)\)[ \n\t]*{(.*)}/gmi;    var match = funcReg.exec(this.replace(/\n/g, " "));    if(match) {      return new Function(match[1].split(","), match[2]);    }    return null;  };}
+        var objPattern = new RegExp(
+            (
+                // Delimiters.
+                "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
 
-                    Object.defineProperty(Array.prototype, name, {
+                // Quoted fields.
+                "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+                // Standard fields.
+                "([^\"\\" + strDelimiter + "\\r\\n]*))"
+            ),
+            "gi"
+            );
+
+        Object.defineProperty(Array.prototype, name, {
 
                         enumerable: false,
 
@@ -428,97 +428,9 @@ function addMethodToArray(name, fn) {
 
                 }
 
-                function itemMatches(item, filter) {
+ 
 
-                    for (var key in filter) {
-
-                        if (filter[key] && filter[key] !== item[key]) return false;
-
-                    }
-
-                    return true;
-
-                }
-
-                addMethodToArray("find", function(filter) {
-
-                    var match = [];
-
-                    for (var i = 0; i < this.length; i++) {
-
-                        if (itemMatches(this[i], filter)) match.push(this[i]);
-
-                    }
-
-                    return match;
-
-                });
-
-                addMethodToArray("findOne", function(filter) {
-
-                    var match = null;
-
-                    for (var i = 0; i < this.length; i++) {
-
-                        if (itemMatches(this[i], filter)) {
-
-                            match = this[i];
-
-                            match.i = i;
-
-                            i = this.length;
-
-                        }
-
-                    }
-
-                    return match;
-
-                }); addMethodToArray("excludes", function(keyword) {
-
-                    return this.indexOf(keyword) == -1;
-
-                }); addMethodToArray("loop", function(fn, o) {
-
-                    if (fn === undefined) return console.log("Please define fn.");
-
-                    if (this === undefined) return console.log("Please define array");
-
-                    o = o || {
-
-                        then: function(fn) {
-
-                            if (!this.resolve) this.resolve = fn;
-
-                        }
-
-                    };
-
-                    o.i === undefined ? o.i = 0 : o.i++;
-
-                    if (!this[o.i]) {
-
-                        if (o.resolve) o.resolve();
-
-                        return o;
-
-                    }
-
-                    var self = this;
-
-                    fn(o.i, this[o.i], function() {
-
-                        setTimeout(function() {
-
-                            self.loop(fn, o);
-
-                        }, 0);
-
-                    });
-
-                    return o;
-
-                }); addMethodToArray("sortByProp", function(prop, descend) {
+                addMethodToArray("sortByProp", function(prop, descend) {
 
                     this.sort(function(a, b) {
 
@@ -535,82 +447,6 @@ function addMethodToArray(name, fn) {
                     });
 
                 });
-
-                if (!Object.loop) {
-
-                    Object.loop = function(obj, fn, parent) {
-
-                        parent = parent || obj;
-
-                        for (var key in obj) {
-
-                            var val = obj[key];
-
-                            if (Array.isArray(val)) {
-
-                                for (var i = 0; i < val.length; i++) {
-
-                                    if (val[i] !== undefined) {
-
-                                        var item = val[i];
-
-                                        typeof item == "object" ? Object.loop(item, fn, parent) : fn(val, i, item, parent);
-
-                                    }
-
-                                }
-
-                            } else if (typeof val === "object" && Object.keys(val).length) {
-
-                                Object.loop(val, fn, parent);
-
-                            } else {
-
-                                fn(obj, key, val, parent);
-
-                            }
-
-                        }
-
-                        return obj;
-
-                    };
-
-                }
-
-                if (!Object.matches) {
-
-                    Object.matches = function(obj1, obj2) {
-
-                        var obj1Keys = Object.keys(obj1),
-
-                            obj2Keys = Object.keys(obj2),
-
-                            shallowKeysMatch = JSON.stringify(obj1Keys) == JSON.stringify(obj2Keys);
-
-                        if (!shallowKeysMatch) return false;
-
-                        var obj1Data = "";
-
-                        Object.loop(obj1, function(obj, key, value) {
-
-                            obj1Data += (key + (value.name || value)) + typeof value;
-
-                        });
-
-                        var obj2Data = "";
-
-                        Object.loop(obj2, function(obj, key, value) {
-
-                            obj2Data += (key + (value.name || value)) + typeof value;
-
-                        });
-
-                        return obj1Data == obj2Data;
-
-                    };
-
-                }
 
                 function section(arr, n) {
 
@@ -642,7 +478,9 @@ function addMethodToArray(name, fn) {
 
                     }
 
-                }; obj.prototype.loop = Object.loop;
+                }; 
+
+                obj.prototype.loop = Object.loop;
 
                 function loop(arr) {
 
